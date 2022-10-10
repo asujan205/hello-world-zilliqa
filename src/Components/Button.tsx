25aafeccc936cee0373ada45a0e6d2575dfb8bdf
Sujan Adhikari <Asujan205@gmail.com>
2022-10-10 21:09:16 +0545

    Button added

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,33 +1,42 @@
-import React from 'react';
+import React from 'react'
+type props ={
+  text:string;
+  onClick(e:any):void;
+  white?: boolean;
+  header?: boolean;
+  modal?: boolean;
+  padding?: boolean;
+  alert?: boolean;
 
 
-type props = {
-    text: string;
-    onClick(e: any): void;
-    white?: boolean;
-    header?: boolean;
-    modal?: boolean;
-    padding?: boolean;
-    alert?: boolean;
-  };
+}
+const Button=(props)=>{
+  const 
+  {text,
+    white = false,
+    modal = false,
+    onClick,
+    padding = false,
+    alert = false,
+  } = props
+  const colours = white
+  ? 'text-gray-900 bg-white'
+  : alert
+  ? 'text-white bg-red-600'
+  : 'text-white bg-gray-900';
+  return(
+    <>
+     <button
+      className={`font-medium py-3 text-sm lg:text-base rounded-button shadow-button ${colours} ${
+        modal ? 'w-full lg:text-base' : 'px-3 lg:px-6'
+      } ${padding ? 'mb-10' : ''}`}
+      {...{ onClick }}
+    >
+      {text}
+    </button>
 
+    </>
+  )
 
-  const Button =(props)=>{
-
-    const {
-        text,
-        white = false,
-        modal = false,
-        onClick,
-        padding = false,
-        alert = false,
-      } = props;
-      const colours = white
-      ? 'text-gray-900 bg-white'
-      : alert
-      ? 'text-white bg-red-600'
-      : 'text-white bg-gray-900';
-      return(
-        
-      )
-  }
+}
+export default Button;
