cac9a87b99290950afa8d21f048c6c63cb3dd383
Sujan Adhikari <Asujan205@gmail.com>
2022-10-11 20:36:10 +0545

    ListingComponentadded

--- src/Components/ListingCard.tsx
+++ src/Components/ListingCard.tsx
@@ -13,3 +13,41 @@ type props={
   user_is_host: boolean;
   onClick(): void;
 }
+ const ListingCard:React.FC<props>=(props)=>{
+    const {id,name,price,rooms,bathrooms,image,renter,rented_till,accumulated_rent,rented,user_is_host,onClick}=props
+    return(
+        <>
+        <div className="w-full rounded-2xl cursor-pointer" onClick={onClick}>
+      <div
+        className="w-full h-48 rounded-lg mb-4 bg-gray-100 flex justify-end items-start p-2"
+        style={{
+          backgroundImage: `url(${image})`,
+          backgroundSize: 'cover',
+        }}
+      >
+        {rented && (
+          <div>
+            <div className="px-2 py-1 bg-gray-200 text-gray-600 rounded uppercase text-xs tracking-wide font-semibold">
+              Unavailable
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex items-center text-base font-light text-gray-600">
+        <p>
+          {rooms} Room{rooms > 1 ? 's' : ''}
+        </p>
+        <div className="w-1 h-1 bg-gray-500 rounded-full mx-2"></div>
+        <p>
+          {bathrooms} Bathroom{bathrooms > 1 ? 's' : ''}
+        </p>
+      </div>
+      <h3 className="text-gray-900 text-xl">{name}</h3>
+      <p className="text-gray-900 font-semibold">
+        {price} ZIL
+        <span className="text-gray-600 font-light"> / night</span>
+      </p>
+    </div>
+        </>
+    )
+ }
