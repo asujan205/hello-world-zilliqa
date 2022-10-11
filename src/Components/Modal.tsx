f02a2c2c4ea6f36e550918c95e32dbdb7a8d33e3
Sujan Adhikari <Asujan205@gmail.com>
2022-10-11 20:18:33 +0545

    added-modal

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -42,6 +42,50 @@ type props ={
 
       </div>
     
+      <div className="w-full h-full flex justify-center items-center px-4 lg:px-2 py-2">
+      <div
+          className="w-full lg:w-1/3 bg-white shadow-xl rounded-2xl max-h-full flex flex-col"
+          onClick={(e) => e.stopPropagation()}
+        >
+
+<div className="flex justify-between items-center p-8">
+            <p className="text-xl font-bold text-gray-900">{title}</p>
+            <button
+              className="p-1 rounded hover:bg-gray-100 transition-colors -mr-1"
+              onClick={() => setVisible(false)}
+            >
+              <svg
+                className="w-6 h-6 text-gray-700"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="w-full px-8 pt-0 overflow-y-scroll flex-grow">
+            {children}
+          </div>
+          <div className="p-8">
+            <Button
+              modal
+              text={buttonText}
+              onClick={(e: any) => {
+                onClick();
+                setVisible(false);
+              }}
+            />
+          </div>
+          
+          </div>
+        </div>
 
     </>)
  }
