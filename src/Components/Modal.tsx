5642a1b1f99d718544fe73b11c3d0c916b1f137f
Sujan Adhikari <Asujan205@gmail.com>
2022-10-11 11:23:40 +0545

    modal added

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -1,3 +1,4 @@
+import { keyboardKey } from '@testing-library/user-event';
 import React,{useEffect} from 'react';
 import Button from './Button';
 type props ={
@@ -9,6 +10,38 @@ type props ={
     onClick(): void;
 }
  const Model :React.FC<props>=(props)=>{
-    return(<>
+    const { title, children, setVisible, visible, buttonText, onClick } = props;
+   useEffect(()=>{
+    document.onkeydown=(e:KeyboardEvent)=>{
+        if(e.key==='Enter'){
+            e.preventDefault();
+            onClick();
+        }
+    }
+   },[])
+   return(<>
+   <div 
+      className={
+        'w-screen h-screen bg-black bg-opacity-25 fixed top-0 left-0 z-20 transition-all'
+      }
+      style={
+        visible
+          ? {
+              opacity: 1,
+              visibility: 'visible',
+              transform: 'translateY(0)',
+            }
+          : {
+              opacity: 0,
+              visibility: 'hidden',
+              transform: 'translateY(30px)',
+            }
+      }
+      onClick={()=>{setVisible(false)}}
+      >
+
+      </div>
+    
+
     </>)
  }
